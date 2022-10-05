6167d9f265881115cffd9da49384bb03296f835d
jehhxuxu <jessica.mariane@ifood.com.br>
2022-10-04 22:22:38 -0300

    validation, formatter, border

---
+++ lib/features/home/widgets/widgets.dart
@@ -0,0 +1,4 @@
+export 'contact.dart';
+export 'my_bottom_bar.dart';
+export 'my_expansion.dart';
+export 'my_grid.dart';
--- lib/features/profile/profile_page.dart
+++ lib/features/profile/profile_page.dart
@@ -1,51 +1,62 @@
 import 'dart:developer';
 
+import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LowerCaseTextFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    return newValue.copyWith(text: newValue.text.toLowerCase());
+  }
+}
 
 class ProfilePage extends StatelessWidget {
   ProfilePage({Key? key}) : super(key: key);
   final secondField = FocusNode();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile Page'),
+        toolbarHeight: 135,
+        elevation: 0,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: TextField(
-              textInputAction: TextInputAction.done,
-              autocorrect: false,
-              style: const TextStyle(color: Colors.green, fontSize: 30),
-
-              onChanged: (value) {
-                log('${value.length}');
-              },
-              // onEditingComplete: () {
-              //   log('passou');
-              //   secondField.requestFocus();
-              // },
-              // onSubmitted: (value) {
-              //   log(value);
-              // },
-              maxLines: 5,
-              //maxLength: 10,
-              //obscureText: true,
-              obscuringCharacter: '*',
-              cursorColor: Colors.pink,
-              decoration: const InputDecoration(),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextFormField(
+                onFieldSubmitted: ((value) {}),
+                textInputAction: TextInputAction.next,
+                validator: (value) {
+                  log(value ?? '');
+                  if (value?.isEmpty ?? false) return 'Campo obrigatório';
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  CepInputFormatter(),
+                ],
+                decoration: const InputDecoration(),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: TextField(
-              focusNode: secondField,
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextFormField(
+                onFieldSubmitted: ((value) {
+                  _formKey.currentState?.reset();
+                }),
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
