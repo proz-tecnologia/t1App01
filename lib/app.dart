12d92f971b9b1d9291ceea23b67e9ed4d3a994ba
jehhxuxu <jessica.mariane@ifood.com.br>
2022-10-13 22:21:43 -0300

    finalizando marvel app, navegacao

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,9 @@
 import 'package:first_app/design_system/my_color.dart';
 import 'package:first_app/design_system/text_theme.dart';
+import 'package:first_app/features/authentication/splash.dart';
+import 'package:first_app/features/home/home_page.dart';
+import 'package:first_app/features/authentication/login_page.dart';
 import 'package:flutter/material.dart';
-
-import 'features/authentication/splash.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,27 +13,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(
-          brightness: Brightness.light,
-          appBarTheme: const AppBarTheme(backgroundColor: MyColor.success),
-          scaffoldBackgroundColor: const Color(0xFF262D2A),
-          textTheme: MyTexTheme.myTheme,
-          textSelectionTheme: const TextSelectionThemeData(
-            cursorColor: Color(0xFFBAC9D4),
-          ),
-          inputDecorationTheme: const InputDecorationTheme(
-            enabledBorder: UnderlineInputBorder(
-              borderSide: BorderSide(
-                color: Color(0xFFBAC9D4),
-              ),
-            ),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(
-                color: Color(0xFFBAC9D4),
-              ),
-            ),
-          )),
       home: const SplashPage(),
+      routes: {
+        'splash': (context) => const SplashPage(),
+        'login': (context) => const LoginPage(),
+        'home': (context) => const HomePage(title: 'Cindi'),
+      },
+      darkTheme: ThemeData(
+        brightness: Brightness.light,
+        appBarTheme: const AppBarTheme(backgroundColor: MyColor.success),
+        scaffoldBackgroundColor: const Color(0xFF262D2A),
+        textTheme: MyTexTheme.myTheme,
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color(0xFFBAC9D4),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Color(0xFFBAC9D4),
+            ),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Color(0xFFBAC9D4),
+            ),
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: const Color(0xFFFFC82C),
+            fixedSize: const Size.fromHeight(30),
+          ),
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+            style: OutlinedButton.styleFrom(
+          primary: const Color(0xFFFFC82C),
+          side: const BorderSide(color: Color(0xFFFFC82C)),
+        )),
+      ),
     );
   }
 }
--- lib/design_system/my_images.dart
+++ lib/design_system/my_images.dart
@@ -1,1 +1,2 @@
 const String img01 = 'assets/images/img01.jpeg';
+const String google = 'assets/images/google.png';
